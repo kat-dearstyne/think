a0e17bf8dbbad3f2a14e691a184fec234037c901
kat-dearstyne <katherine@jklfamily.com>
2021-01-02 17:37:22 -0500

    fix so no feedback is given for transfer phase

--- examples/face_classification.py
+++ examples/face_classification.py
@@ -21,14 +21,16 @@ class Condition:  # TODO where should this go
 class FaceClassificationTask(Task):
     N_BLOCKS = {'Training': 12, 'Transfer': 2}
     FACES = [Face(23.5, 21.5, 13.5, 16.5), Face(19.5, 11.5, 18, 12), Face(23.5, 16.5, 13.5, 12), Face(23.5, 16.5, 18, 12),
-             Face(23.5, 16.5, 18, 7.5), Face(15, 11.5, 9, 16.5), Face(19.5, 16.5, 9, 7.5), Face(15, 11.5, 18, 16.5),
-             Face(15, 11.5, 18, 7.5), Face(15, 11.5, 9, 7.5), Face(19.5, 21.5, 9, 7.5), Face(23.5, 11.5, 9, 7.5),
-             Face(15, 21.5, 13.5, 12), Face(19.5, 16.5, 9, 16.5), Face(19.5, 16.5, 9, 12), Face(15, 16.5, 13.5, 16.5),
-             Face(23.5, 11.5, 9, 16.5), Face(15, 11.5, 13.5, 12), Face(15, 21.5, 18, 16.5), Face(19.5, 11.5, 18, 16.5),
-             Face(19.5, 21.5, 9, 7.5), Face(15, 21.5, 18, 12), Face(19.5, 11.5, 13.5, 16.5), Face(15, 16.5, 13.5, 12),
-             Face(15, 16.5, 18, 16.5), Face(19.5, 21.5, 9, 16.5), Face(15, 16.5, 18, 12), Face(19.5, 16.5, 13.5, 7.5),
+             # 4
+             Face(23.5, 16.5, 18, 7.5), Face(15, 11.5, 9, 16.5), Face(19.5, 16.5, 9, 7.5), Face(15, 11.5, 18, 16.5),  # 8
+             Face(15, 11.5, 18, 7.5), Face(15, 11.5, 9, 7.5), Face(19.5, 21.5, 9, 7.5), Face(23.5, 11.5, 9, 7.5),  # 12
+             Face(15, 21.5, 13.5, 12), Face(19.5, 16.5, 9, 16.5), Face(19.5, 16.5, 9, 12), Face(15, 16.5, 13.5, 16.5),  # 16
+             Face(23.5, 11.5, 9, 16.5), Face(15, 11.5, 13.5, 12), Face(15, 21.5, 18, 16.5), Face(19.5, 11.5, 18, 16.5),  # 20
+             Face(19.5, 21.5, 9, 7.5), Face(15, 21.5, 18, 12), Face(19.5, 11.5, 13.5, 16.5), Face(15, 16.5, 13.5, 12),  # 24
+             Face(15, 16.5, 18, 16.5), Face(19.5, 21.5, 9, 16.5), Face(15, 16.5, 18, 12), Face(19.5, 16.5, 13.5, 7.5),  # 28
              Face(15, 21.5, 13.5, 16.5), Face(15, 11.5, 13.5, 12), Face(23.5, 11.5, 18, 12), Face(19.5, 11.5, 13.5, 12),
-             Face(22.7, 16.5, 16.2, 12), Face(15.9, 12.5, 12.6, 11.1)]
+             # 32
+             Face(22.7, 16.5, 16.2, 12), Face(15.9, 12.5, 12.6, 11.1)]  # 34
     CAT = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 2]
 
     def __init__(self, env, condition, corrects=None):
@@ -63,13 +65,16 @@ class FaceClassificationTask(Task):
                     self.display.set_attend(face_visual)
                     self.wait(5)
                     self.display.clear()
-                    category_visual = self.display.add_text(50, 50, self.CAT[num])
-                    self.display.set_attend(category_visual)
+                    if is_training():
+                        category_visual = self.display.add_text(50, 50, self.CAT[num])
+                        self.display.set_attend(category_visual)
+                    else:
+                        self.display.add_text(50, 50, '')
 
 
 class FaceClassificationAgent(Agent):
-    SIM_WEIGHTS = [1.3, 1.3, .7, .7]
-    MAX_MIN = [(23.5, 15), (21.5, 11.5), (18, 9), (16.5, 7.5)]  # TODO move somewhere?
+    SIM_WEIGHTS = [.19, .12, .25, .45]
+    MAX_MIN = [(23.5, 15), (21.5, 11.5), (18, 9), (16.5, 7.5)]  # TODO move somewhere? calculate instead of hard code?
 
     def __init__(self, env, output=False):
         super().__init__(output=output)
@@ -98,29 +103,32 @@ class FaceClassificationAgent(Agent):
             face = self.vision.encode(visual)
             chunk = self.memory.recall(distances=self._get_similarities(), eh=face.eh, es=face.es, nl=face.nl, mh=face.mh)
             if chunk:
-                self.motor.type(chunk.get('category'))
+                selected_category = chunk.get('category')
             else:
-                self.motor.type(self.guess())
+                selected_category = self.guess()
+            self.motor.type(selected_category)
             visual = self.vision.wait_for(isa='text')
             category = self.vision.encode(visual)
+            if not category:
+                category = selected_category
             self.memory.store(eh=face.eh, es=face.es, nl=face.nl, mh=face.mh, category=category)
 
 
 class FaceClassificationSimulation:
     TRAINING_STIMULI = {1: [i for i in range(10)], 2: [8, 12, 17, 19, 22, 23, 25, 27, 28, 32]}
-    CONDITIONS = [Condition('1A', TRAINING_STIMULI[1]), Condition('1B', TRAINING_STIMULI[1], exe_num=7),
-                  Condition('EF', TRAINING_STIMULI[2]), Condition('HF19', TRAINING_STIMULI[2], exe_num=19)]
+    CONDITIONS = [Condition('1EF', TRAINING_STIMULI[1]), Condition('1HF7', TRAINING_STIMULI[1], exe_num=7),
+                  Condition('2EF', TRAINING_STIMULI[2]), Condition('2HF19', TRAINING_STIMULI[2], exe_num=19)]
     HUMAN_CORRECT = {
-        '1A': [.968, .665, .937, .975, .875, .134, .261, .171, .089, .045, .529, .538, .258, .541, .430, .283, .778,
+        '1EF': [.968, .665, .937, .975, .875, .134, .261, .171, .089, .045, .529, .538, .258, .541, .430, .283, .778,
                .145, .281, .780, .291, .277, .601, .289, .272, .490, .261, .386, .269, .114, .796, .538, .981, .126],
-        '1B': [.956, .594, .931, .944, .775, .1, .1, .15, .069, .006, .4, .488, .275, .425, .238, .269, .675,
+        '1HF7': [.956, .594, .931, .944, .775, .1, .1, .15, .069, .006, .4, .488, .275, .425, .238, .269, .675,
                .094, .325, .681, .125, .269, .45, .219, .275, .413, .275, .144, .294, .094, .806, .300, .931, .05],
-        'EF': [.660, .253, .456, .375, .843, .156, .855, .113, .717, .681, .711, .763, .937, .338, .525, .394, .081,
+        '2EF': [.660, .253, .456, .375, .843, .156, .855, .113, .717, .681, .711, .763, .937, .338, .525, .394, .081,
                .381, .774, .095, .944, .925, .094, .633, .394, .744, .675, .844, .838, .318, .27, .283, .369, .488],
-        'HF19': [.748, .151, .43, .415, .698, .101, .881, .126, .516, .541, .881, .656, .956, .516, .66, .679, .031,
+        '2HF19': [.748, .151, .43, .415, .698, .101, .881, .126, .516, .541, .881, .656, .956, .516, .66, .679, .031,
                  .189, .963, .069, .938, .969, .044, .811, .675, .855, .731, .844, .950, .222, .126, .169, .409, .264]}
 
-    def run(self, n_trials=50, output=False, real_time=False, print_results=False, show_experiment=True):
+    def run(self, n_trials=80, output=False, real_time=False, print_results=False, show_experiment=True):
 
         for condition in self.CONDITIONS:
             selected_categories = Data(len(FaceClassificationTask.FACES))
