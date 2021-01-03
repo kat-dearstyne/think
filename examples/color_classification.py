33ae3afd6101ff03fb6493d4a4d4acdcdffe06ee
kat-dearstyne <katherine@jklfamily.com>
2021-01-02 21:05:33 -0500

    refactor and add additional conditions

--- examples/color_classification.py
+++ examples/color_classification.py
@@ -6,17 +6,19 @@ random.seed(20)
 
 
 class Condition:  # TODO where should this go
-    BASE_FREQ = 4
 
-    def __init__(self, name, train_stim, exe_num=-1, exe_freq=19):
+    def __init__(self, name, base_train_stim, exe_num=-1, exe_freq=19, base_freq=4, sim_weights=(0, 1.17, .83)):
         self.name = name
-        self.train_stim = self._create_training_stimuli_nums(train_stim, exe_num, exe_freq)
+        self.sim_weights = sim_weights
+        self.train_stim = self._create_training_stimuli_nums(base_train_stim, exe_num, exe_freq, base_freq)
 
-    def _create_training_stimuli_nums(self, train_stim, exe_num, exe_freq):
-        train_stim = train_stim.copy()
+    def _create_training_stimuli_nums(self, base_stim, exe_num, exe_freq, base_freq):
+        train_stim = []
+        for i in range(base_freq):
+            train_stim.extend(base_stim.copy())
         if exe_num >= 0:
-            train_stim.extend([exe_num for i in range(exe_freq - 1)])
-        return train_stim
+            train_stim.extend([exe_num for i in range(exe_freq - base_freq)])
+        return base_stim
 
 
 class ColorClassificationTask(Task):
@@ -24,7 +26,7 @@ class ColorClassificationTask(Task):
     COLORS = [Color(s=15, l=64), Color(s=56, l=74), Color(s=32, l=59), Color(s=65, l=65), Color(s=18, l=51),
               Color(s=46, l=51), Color(s=78, l=48), Color(s=32, l=42), Color(s=64, l=42), Color(s=12, l=29),
               Color(s=52, l=26), Color(s=65, l=26)]
-    CAT = [1, 2, 2, 2, 1, 2, 2, 1, 2, 1, 1, 1]  # (color, category) pairs
+    CAT = [1, 2, 2, 2, 1, 2, 2, 1, 2, 1, 1, 1]
 
     def __init__(self, env, condition, corrects=None):
         super().__init__()
@@ -65,11 +67,9 @@ class ColorClassificationTask(Task):
 class ColorClassificationAgent(Agent):
     KNOWLEDGE = {1: [Color(11, 45, 35)], 2: [Color(350, 100, 88)]}
     KNOWLEDGE_STRENGTH = 1
-    SIM_WEIGHTS = [0, 1.17, .83]
 
-    def __init__(self, env, output=False, demo_blending=False):
+    def __init__(self, env, condition, output=False):
         super().__init__(output=output)
-        self.demo_blending = demo_blending
         self.memory = Memory(self, Memory.OPTIMIZED_DECAY)
         self.vision = Vision(self, env.display)
         self.motor = Motor(self, self.vision, env)
@@ -77,7 +77,8 @@ class ColorClassificationAgent(Agent):
         self.memory.activation_noise = .5
         self.memory.retrieval_threshold = -1.8
         self.memory.latency_factor = .450
-        self.memory.match_scale = 5
+        self.memory.match_scale = 10
+        self.condition = condition
         self._save_knowledge_to_memory()
 
     def _save_knowledge_to_memory(self):
@@ -91,9 +92,9 @@ class ColorClassificationAgent(Agent):
 
     def _get_distances(self):
         return {
-            'h': lambda a, b: self.calculate_distances_hue(a, b, self.SIM_WEIGHTS[0]),
-            's': lambda a, b: self.calculate_distances_other(a, b, self.SIM_WEIGHTS[1]),
-            'l': lambda a, b: self.calculate_distances_other(a, b, self.SIM_WEIGHTS[2])
+            'h': lambda a, b: self.calculate_distances_hue(a, b, self.condition.sim_weights[0]),
+            's': lambda a, b: self.calculate_distances_other(a, b, self.condition.sim_weights[1]),
+            'l': lambda a, b: self.calculate_distances_other(a, b, self.condition.sim_weights[2])
         }
 
     def calculate_distances_other(self, val1, val2, w=1.0):
@@ -116,24 +117,24 @@ class ColorClassificationAgent(Agent):
             chunk = self.memory.recall(distances=self._get_distances(), h=color.h,
                                        s=color.s, l=color.l)
             if chunk:
-                self.motor.type(chunk.get('category'))
+                selected_category = chunk.get('category')
             else:
-                self.motor.type(self.guess_bias())
+                selected_category = self.guess_bias()
+            self.motor.type(selected_category)
             visual = self.vision.wait_for(isa='text')
             category = self.vision.encode(visual)
             self.memory.store(h=color.h, s=color.s, l=color.l, category=category)
-            if self.demo_blending:
-                blended_chunk = self.memory.get_blended_chunk(slots='s', distances=self._get_distances(), h=color.h,
-                                                              l=color.l, category=category)
-                print(blended_chunk)
 
 
 class ColorClassificationSimulation:
     TRAIN_STIM = [i for i in range(len(ColorClassificationTask.COLORS))]
-    CONDITIONS = [Condition('B', TRAIN_STIM), Condition('E2', TRAIN_STIM, exe_num=1), Condition('E7', TRAIN_STIM, exe_num=6)]
-    HUMAN_CORRECT = {'B': [.318, .123, .513, .113, .175, .337, .13, .162, .372, .097, .143, .272],
+    CONDITIONS = [Condition('B1', TRAIN_STIM), Condition('E2', TRAIN_STIM, exe_num=1), Condition('E7', TRAIN_STIM, exe_num=6),
+                  Condition('E6(3)', TRAIN_STIM, exe_num=5, exe_freq=12), Condition('E6(5)', TRAIN_STIM, exe_num=5)]
+    HUMAN_CORRECT = {'B1': [.318, .123, .513, .113, .175, .337, .13, .162, .372, .097, .143, .272],
                      'E2': [.296, .026, .46, .067, .114, .328, .181, .116, .409, .05, .103, .223],
-                     'E7': [.308, .147, .555, .103, .16, .384, .039, .131, .345, .066, .146, .267]}
+                     'E7': [.308, .147, .555, .103, .16, .384, .039, .131, .345, .066, .146, .267],
+                     'E6(3)': [.332, .155, .420, .118, .195, .208, .167, .168, .328, .115, .169, .254],
+                     'E6(5)': [.288, .141, .415, .119, .19, .123, .157, .167, .336, .087, .108, .182]}
 
     def run(self, n_trials=50, output=False, real_time=False, print_results=False, show_experiment=True):
 
@@ -143,8 +144,8 @@ class ColorClassificationSimulation:
             for _ in range(n_trials):
                 env = Environment(window=(500, 500) if show_experiment else None)
                 task = ColorClassificationTask(env, condition, corrects=corrects)
-                agent = ColorClassificationAgent(env)
-                World(task, agent).run(1590, output=output, real_time=real_time)
+                agent = ColorClassificationAgent(env, condition)
+                World(task, agent).run(1590, output=output, real_time=real_time)  # TODO figure out time
 
             learning_error = Result(corrects.proportion(0), self.HUMAN_CORRECT[condition.name])
 
--- examples/face_classification.py
+++ examples/face_classification.py
@@ -7,31 +7,33 @@ random.seed(20)
 
 class Condition:  # TODO where should this go
 
-    def __init__(self, name, train_stim, exe_num=-1, exe_freq=5):
+    def __init__(self, name, base_stim, exe_num=-1, exe_freq=5, base_freq=1, sim_weights=None):
         self.name = name
-        self.train_stim = self._create_training_stimuli_nums(train_stim, exe_num, exe_freq)
+        self.sim_weights = sim_weights
+        self.train_stim = self._create_training_stimuli_nums(base_stim, exe_num, exe_freq, base_freq)
 
-    def _create_training_stimuli_nums(self, train_stim, exe_num, exe_freq):
-        train_stim = train_stim.copy()
+    def _create_training_stimuli_nums(self, base_stim, exe_num, exe_freq, base_freq):
+        train_stim = []
+        for i in range(base_freq):
+            train_stim.extend(base_stim.copy())
         if exe_num >= 0:
-            train_stim.extend([exe_num for i in range(exe_freq - 1)])
-        return train_stim
+            train_stim.extend([exe_num for i in range(exe_freq - base_freq)])
+        return base_stim
 
 
 class FaceClassificationTask(Task):
     N_BLOCKS = {'Training': 12, 'Transfer': 2}
-    FACES = [Face(23.5, 21.5, 13.5, 16.5), Face(19.5, 11.5, 18, 12), Face(23.5, 16.5, 13.5, 12), Face(23.5, 16.5, 18, 12),
-             # 4
+    FACES = [Face(23.5, 21.5, 13.5, 16.5), Face(19.5, 11.5, 18, 12), Face(23.5, 16.5, 13.5, 12), Face(23.5, 16.5, 18, 12),  # 4
              Face(23.5, 16.5, 18, 7.5), Face(15, 11.5, 9, 16.5), Face(19.5, 16.5, 9, 7.5), Face(15, 11.5, 18, 16.5),  # 8
-             Face(15, 11.5, 18, 7.5), Face(15, 11.5, 9, 7.5), Face(19.5, 21.5, 9, 7.5), Face(23.5, 11.5, 9, 7.5),  # 12
+             Face(15, 11.5, 18, 7.5), Face(15, 11.5, 9, 7.5), Face(19.5, 21.5, 9, 16.5), Face(23.5, 11.5, 9, 7.5),  # 12
              Face(15, 21.5, 13.5, 12), Face(19.5, 16.5, 9, 16.5), Face(19.5, 16.5, 9, 12), Face(15, 16.5, 13.5, 16.5),  # 16
              Face(23.5, 11.5, 9, 16.5), Face(15, 11.5, 13.5, 12), Face(15, 21.5, 18, 16.5), Face(19.5, 11.5, 18, 16.5),  # 20
              Face(19.5, 21.5, 9, 7.5), Face(15, 21.5, 18, 12), Face(19.5, 11.5, 13.5, 16.5), Face(15, 16.5, 13.5, 12),  # 24
              Face(15, 16.5, 18, 16.5), Face(19.5, 21.5, 9, 16.5), Face(15, 16.5, 18, 12), Face(19.5, 16.5, 13.5, 7.5),  # 28
-             Face(15, 21.5, 13.5, 16.5), Face(15, 11.5, 13.5, 12), Face(23.5, 11.5, 18, 12), Face(19.5, 11.5, 13.5, 12),
-             # 32
+             Face(15, 21.5, 13.5, 16.5), Face(15, 11.5, 13.5, 12), Face(23.5, 11.5, 18, 12), Face(19.5, 11.5, 13.5, 12),  # 32
              Face(22.7, 16.5, 16.2, 12), Face(15.9, 12.5, 12.6, 11.1)]  # 34
-    CAT = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 2]
+    CAT_E1 = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 2]
+    CAT_E2 = [2, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 2, 2, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 2, 2, 2, 1, 1]
 
     def __init__(self, env, condition, corrects=None):
         super().__init__()
@@ -66,17 +68,17 @@ class FaceClassificationTask(Task):
                     self.wait(5)
                     self.display.clear()
                     if is_training():
-                        category_visual = self.display.add_text(50, 50, self.CAT[num])
+                        category = self.CAT_E1[num] if self.condition.name[0] == '1' else self.CAT_E2[num]
+                        category_visual = self.display.add_text(50, 50, category)
                         self.display.set_attend(category_visual)
                     else:
                         self.display.add_text(50, 50, '')
 
 
 class FaceClassificationAgent(Agent):
-    SIM_WEIGHTS = [.19, .12, .25, .45]
     MAX_MIN = [(23.5, 15), (21.5, 11.5), (18, 9), (16.5, 7.5)]  # TODO move somewhere? calculate instead of hard code?
 
-    def __init__(self, env, output=False):
+    def __init__(self, env, condition, output=False, demo_blending=False):
         super().__init__(output=output)
         self.memory = Memory(self, Memory.OPTIMIZED_DECAY)
         self.vision = Vision(self, env.display)
@@ -86,12 +88,14 @@ class FaceClassificationAgent(Agent):
         self.memory.retrieval_threshold = -1.8
         self.memory.latency_factor = .450
         self.memory.match_scale = 10
+        self.condition = condition
+        self.demo_blending = demo_blending
 
-    def _get_similarities(self):
-        return {Face.features[i]: lambda a, b: self.calculate_similarity(a, b, self.MAX_MIN[i], self.SIM_WEIGHTS[i])
+    def _get_distances(self):
+        return {Face.features[i]: lambda a, b: self.calculate_differences(a, b, self.MAX_MIN[i], self.condition.sim_weights[i])
                 for i in range(len(Face.features))}
 
-    def calculate_similarity(self, slot1, slot2, max_min, w=1.0):
+    def calculate_differences(self, slot1, slot2, max_min, w=1.0):
         return (abs(slot1 - slot2) / (max_min[0] - max_min[1])) * w
 
     def guess(self):
@@ -101,7 +105,7 @@ class FaceClassificationAgent(Agent):
         while self.time() < time:
             visual = self.vision.wait_for(isa='face')
             face = self.vision.encode(visual)
-            chunk = self.memory.recall(distances=self._get_similarities(), eh=face.eh, es=face.es, nl=face.nl, mh=face.mh)
+            chunk = self.memory.recall(distances=self._get_distances(), eh=face.eh, es=face.es, nl=face.nl, mh=face.mh)
             if chunk:
                 selected_category = chunk.get('category')
             else:
@@ -112,23 +116,29 @@ class FaceClassificationAgent(Agent):
             if not category:
                 category = selected_category
             self.memory.store(eh=face.eh, es=face.es, nl=face.nl, mh=face.mh, category=category)
+            if self.demo_blending:
+                blended_chunk = self.memory.get_blended_chunk(slots='eh', distances=self._get_distances(),
+                                                              es=face.es, nl=face.nl, mh=face.mh, category=category)
+                print(blended_chunk)
 
 
 class FaceClassificationSimulation:
     TRAINING_STIMULI = {1: [i for i in range(10)], 2: [8, 12, 17, 19, 22, 23, 25, 27, 28, 32]}
-    CONDITIONS = [Condition('1EF', TRAINING_STIMULI[1]), Condition('1HF7', TRAINING_STIMULI[1], exe_num=7),
-                  Condition('2EF', TRAINING_STIMULI[2]), Condition('2HF19', TRAINING_STIMULI[2], exe_num=19)]
+    CONDITIONS = [Condition('1EF', TRAINING_STIMULI[1], sim_weights=(.19, .12, .25, .45)),
+                  Condition('1HF7', TRAINING_STIMULI[1], exe_num=7, sim_weights=(.15, .15, .29, .41)),
+                  Condition('2EF', TRAINING_STIMULI[2], sim_weights=(.108, .463, 0, .429)),
+                  Condition('2HF19', TRAINING_STIMULI[2], exe_num=19, sim_weights=(.126, .587, 0, .288))]
     HUMAN_CORRECT = {
         '1EF': [.968, .665, .937, .975, .875, .134, .261, .171, .089, .045, .529, .538, .258, .541, .430, .283, .778,
-               .145, .281, .780, .291, .277, .601, .289, .272, .490, .261, .386, .269, .114, .796, .538, .981, .126],
+                .145, .281, .780, .291, .277, .601, .289, .272, .490, .261, .386, .269, .114, .796, .538, .981, .126],
         '1HF7': [.956, .594, .931, .944, .775, .1, .1, .15, .069, .006, .4, .488, .275, .425, .238, .269, .675,
-               .094, .325, .681, .125, .269, .45, .219, .275, .413, .275, .144, .294, .094, .806, .300, .931, .05],
+                 .094, .325, .681, .125, .269, .45, .219, .275, .413, .275, .144, .294, .094, .806, .300, .931, .05],
         '2EF': [.660, .253, .456, .375, .843, .156, .855, .113, .717, .681, .711, .763, .937, .338, .525, .394, .081,
-               .381, .774, .095, .944, .925, .094, .633, .394, .744, .675, .844, .838, .318, .27, .283, .369, .488],
+                .381, .774, .095, .944, .925, .094, .633, .394, .744, .675, .844, .838, .318, .27, .283, .369, .488],
         '2HF19': [.748, .151, .43, .415, .698, .101, .881, .126, .516, .541, .881, .656, .956, .516, .66, .679, .031,
-                 .189, .963, .069, .938, .969, .044, .811, .675, .855, .731, .844, .950, .222, .126, .169, .409, .264]}
+                  .189, .963, .069, .938, .969, .044, .811, .675, .855, .731, .844, .950, .222, .126, .169, .409, .264]}
 
-    def run(self, n_trials=80, output=False, real_time=False, print_results=False, show_experiment=True):
+    def run(self, n_trials=80, output=False, real_time=False, print_results=False, show_experiment=True, demo_blending=False):
 
         for condition in self.CONDITIONS:
             selected_categories = Data(len(FaceClassificationTask.FACES))
@@ -136,7 +146,7 @@ class FaceClassificationSimulation:
             for _ in range(n_trials):
                 env = Environment(window=(500, 500) if show_experiment else None)
                 task = FaceClassificationTask(env, condition, corrects=selected_categories)
-                agent = FaceClassificationAgent(env)
+                agent = FaceClassificationAgent(env, condition, demo_blending=demo_blending)
                 World(task, agent).run(2400, output=output, real_time=real_time)
 
             prob_cat1 = Result(selected_categories.proportion(1), self.HUMAN_CORRECT[condition.name])
@@ -147,4 +157,5 @@ class FaceClassificationSimulation:
 
 
 if __name__ == '__main__':
-    FaceClassificationSimulation().run(output=False, real_time=False, print_results=True, show_experiment=False)
+    FaceClassificationSimulation().run(output=False, real_time=False, print_results=True, show_experiment=False,
+                                       demo_blending=False)
